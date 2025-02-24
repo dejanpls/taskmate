eb9af5fea5d1639c8e180368b84c9ec6f98d4488
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-02-24 08:38:55 +0100

    feature: Implement localStorage logic for task serialization/deserialization

---
+++ src/localStorage.js
@@ -0,0 +1,26 @@
+import Tasks from "./tasks.js";
+import Task from "./task.js";
+import {format} from 'date-fns';
+
+export default class LocalStorage {
+    static saveTasks() {
+        
+        const taskData = Tasks.list.map(task => ({
+            id: task.id,  // Save ID
+            title: task.title,
+            description: task.description,
+            dueDate: task.dueDate,
+            priority: task.priority,
+            status: task.status
+        }));
+        console.log("Saved tasks: ", taskData);
+        localStorage.setItem('tasks', JSON.stringify(taskData));
+    }
+
+    static loadTasks() {
+        const storedTasks = localStorage.getItem('tasks');
+        const parsedTasks = storedTasks ? JSON.parse(storedTasks) : [];
+    
+        return parsedTasks.map(taskData => Task.fromData(taskData));
+    }        
+}
