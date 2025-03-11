286bb3e6bb9eecf17d9bad36c81214f0536a038f
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-03-11 08:47:21 +0100

    refactor: include status argument to display error messages accordingly

--- src/js/ui/log.js
+++ src/js/ui/log.js
@@ -2,12 +2,15 @@ import UI from "./ui.js";
 
 export default class Log {
 
-    static notify(message) {
-        const inputInfo = UI.generateInputInfo(message);
+    static notify(message, status = 'success') {
+        const inputInfo = UI.generateInputInfo(message, status);
 
         setTimeout(() => {
             inputInfo.remove();
         }, 1500); // clears notification after 1.5 seconds;
     }
 
+    static truncate(str, maxLength) {
+        return str.length > maxLength ? str.slice(0, maxLength) + "..." : str;
+    }
 }
