6ee387f5507bb2ef08dc4e19d699bfa7ecd1b89d
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-06 12:11:17 +0200

    feat(App): create handleChange to update newTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,86 @@
+import { useState } from 'react';
+
 import Categories from './components/Categories.jsx';
+import { categories } from './utils/categories.js';
+import { getCurrentDateFormatted } from './utils/dateConverter.js';
 
 export default function App() {
-  const categories = ['default', 'work', 'leisure'];
+  const initialTask = {
+    id: Date.now(),
+    title: '',
+    description: '',
+    dueDate: getCurrentDateFormatted(),
+    priority: 'low',
+    category: 'default',
+  };
+
+  const [newTask, setNewTask] = useState(initialTask);
+  const [tasks, setTasks] = useState([]);
+  const currentCategory = newTask.category;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setNewTask((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   return (
-    <form>
-      <label htmlFor="title">
-        <input name="title" id="title" type="text" />
-      </label>
+    <>
+      <form>
+        <label htmlFor="title">
+          <input
+            value={newTask.title}
+            onChange={handleChange}
+            name="title"
+            id="title"
+            type="text"
+          />
+        </label>
 
-      <label htmlFor="description">
-        Description:
-        <textarea name="description" id="description" />
-      </label>
+        <label htmlFor="description">
+          Description:
+          <textarea
+            value={newTask.description}
+            onChange={handleChange}
+            name="description"
+            id="description"
+          />
+        </label>
 
-      <label htmlFor="due-date">
-        Due date:
-        <input type="date" name="due-date" id="due-date" />
-      </label>
+        <label htmlFor="dueDate">
+          Due date:
+          <input
+            value={newTask.dueDate}
+            onChange={handleChange}
+            type="date"
+            name="dueDate"
+            id="dueDate"
+          />
+        </label>
 
-      <label htmlFor="priority">
-        Priority:
-        <select name="priority" id="priority">
-          <option value="low">Low</option>
-          <option value="medium">Medium</option>
-          <option value="high">High</option>
-        </select>
-      </label>
+        <label htmlFor="priority">
+          Priority:
+          <select
+            value={newTask.priority}
+            onChange={handleChange}
+            name="priority"
+            id="priority"
+          >
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </label>
 
-      <Categories {...{ categories }} />
+        <Categories {...{ handleChange, currentCategory, categories }} />
 
-      <button type="submit">Add Task</button>
-    </form>
+        <button type="submit">Add Task</button>
+      </form>
+
+      {console.log(newTask)}
+    </>
   );
 }
