fcad5676b3bbe08f46247a742439b5e0c279257c
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-06 12:31:40 +0200

    feat(Form): create component

---
+++ src/components/Form.jsx
@@ -0,0 +1,61 @@
+import Categories from './Categories.jsx';
+
+export default function Form({
+  handleSubmit,
+  newTask,
+  handleChange,
+  categoryData,
+}) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">
+        <input
+          value={newTask.title}
+          onChange={handleChange}
+          name="title"
+          id="title"
+          type="text"
+        />
+      </label>
+
+      <label htmlFor="description">
+        Description:
+        <textarea
+          value={newTask.description}
+          onChange={handleChange}
+          name="description"
+          id="description"
+        />
+      </label>
+
+      <label htmlFor="dueDate">
+        Due date:
+        <input
+          value={newTask.dueDate}
+          onChange={handleChange}
+          type="date"
+          name="dueDate"
+          id="dueDate"
+        />
+      </label>
+
+      <label htmlFor="priority">
+        Priority:
+        <select
+          value={newTask.priority}
+          onChange={handleChange}
+          name="priority"
+          id="priority"
+        >
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+      </label>
+
+      <Categories {...{ handleChange, ...categoryData }} />
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
