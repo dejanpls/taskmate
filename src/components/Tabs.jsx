9afcfb68877344a651e39d946b8088508e473481
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-07 14:54:16 +0200

    feat(Tabs): create component

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,10 @@
+export default function Tabs() {
+  return (
+    <div>
+      <button type="button">All</button>
+      <button type="button">Active</button>
+      <button type="button">Done</button>
+      <button type="button">Deleted</button>
+    </div>
+  );
+}
