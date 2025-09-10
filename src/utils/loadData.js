78ba0a690ddc752fd70eecba6be56a73bc17ae32
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-10 11:49:06 +0200

    feat(loadData): add loadString to retrieve string from localStorage

--- src/utils/loadData.js
+++ src/utils/loadData.js
@@ -2,3 +2,8 @@ export const loadObject = (obj, fallback) => {
   const saved = localStorage.getItem(obj);
   return saved ? JSON.parse(saved) : fallback;
 };
+
+export const loadString = (str, fallback) => {
+  const saved = localStorage.getItem(str);
+  return saved ? saved : fallback;
+};
