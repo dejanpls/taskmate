be911a173517ae822ef44c4f5d26e45fa65436d2
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-11 11:50:46 +0200

    feat(Deleted): add delete forever button

--- src/components/Deleted.jsx
+++ src/components/Deleted.jsx
@@ -1,14 +1,30 @@
-export default function Deleted({ deletedTasks }) {
+export default function Deleted({
+  handleRestore,
+  handleDeleteForever,
+  deletedTasks,
+}) {
   return (
     <>
-      <ul>
-        {deletedTasks.map((task) => (
-          <li key={task.id}>
-            <span>{task.title}</span>
-            <button type="button">Restore</button>
-          </li>
-        ))}
-      </ul>
+      {deletedTasks.length > 0 ? (
+        <ul>
+          {deletedTasks.map((task) => (
+            <li key={task.id}>
+              <span>{task.title}</span>
+              <button
+                onClick={() => handleDeleteForever(task.id)}
+                type="button"
+              >
+                Delete Forever
+              </button>
+              <button onClick={() => handleRestore(task)} type="button">
+                Restore
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tasks available</p>
+      )}
     </>
   );
 }
