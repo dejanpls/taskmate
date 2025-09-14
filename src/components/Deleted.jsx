c9ba3f647c5d8a079125744bddd651bbd5adf697
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-14 13:23:23 +0200

    feat(Deleted): apply flex layout

--- src/components/Deleted.jsx
+++ src/components/Deleted.jsx
@@ -1,30 +1,40 @@
+import '../styles/deleted.css';
+
 export default function Deleted({
   handleRestore,
   handleDeleteForever,
   deletedTasks,
 }) {
   return (
-    <>
+    <div className="deletedContainer">
+      <h2>Deleted Tasks</h2>
       {deletedTasks.length > 0 ? (
         <ul>
           {deletedTasks.map((task) => (
             <li key={task.id}>
               <span>{task.title}</span>
-              <button
-                onClick={() => handleDeleteForever(task.id)}
-                type="button"
-              >
-                Delete Forever
-              </button>
-              <button onClick={() => handleRestore(task)} type="button">
-                Restore
-              </button>
+              <div className="buttonContainer">
+                <button
+                  onClick={() => handleDeleteForever(task.id)}
+                  type="button"
+                  className="material-icons"
+                >
+                  delete_forever
+                </button>
+                <button
+                  className="material-icons"
+                  onClick={() => handleRestore(task)}
+                  type="button"
+                >
+                  undo
+                </button>
+              </div>
             </li>
           ))}
         </ul>
       ) : (
         <p>No tasks available</p>
       )}
-    </>
+    </div>
   );
 }
