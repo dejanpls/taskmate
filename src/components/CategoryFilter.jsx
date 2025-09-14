30f4e003c35bdc5123d03b2f7577b8b7dc3a4663
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-14 13:37:21 +0200

    feat(CategoryFilter): apply flex layout

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,3 +1,5 @@
+import '../styles/categoryFilter.css';
+
 export default function CategoryFilter({
   handleCategoriesChange,
   selectedCategories,
@@ -6,23 +8,29 @@ export default function CategoryFilter({
   handleDeleteCategory,
 }) {
   return (
-    <div>
+    <div className="categoryFilterContainer">
       <ul>
+        <h2>Selected Categories</h2>
         {categories.map((category) => (
           <li key={`show-${category}`}>
-            <input
-              type="checkbox"
-              checked={selectedCategories.includes(category)}
-              onChange={() => handleCategoriesChange(category)}
-            />
-            <span>{category}</span>
-            <button
-              disabled={categoryInUse(category)}
-              onClick={() => handleDeleteCategory(category)}
-              type="button"
-            >
-              Delete
-            </button>
+            <div className="primaryContainer">
+              <input
+                type="checkbox"
+                checked={selectedCategories.includes(category)}
+                onChange={() => handleCategoriesChange(category)}
+              />
+              <span>{category}</span>
+            </div>
+            <div className="buttonContainer">
+              <button
+                className="material-icons"
+                disabled={categoryInUse(category)}
+                onClick={() => handleDeleteCategory(category)}
+                type="button"
+              >
+                delete_forever
+              </button>
+            </div>
           </li>
         ))}
       </ul>
