15a4be7b1d3321ef9055a71d17b3b8ff8e051d4f
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-14 09:30:31 +0200

    style(TaskList): adjust layout and add checkbox effects

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,3 +1,4 @@
+import '../styles/taskList.css';
 import { prettyDate } from '../utils/prettyDate.js';
 
 export default function TaskList({
@@ -10,34 +11,46 @@ export default function TaskList({
   return (
     <>
       {getProcessedTasks().length > 0 ? (
-        <ul>
+        <ul className="list">
           {getProcessedTasks().map((task) => (
-            <li key={task.id}>
-              <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => handleCompleted(task.id)}
-              />
-              <h2>{task.title}</h2>
+            <li className="listItem" key={task.id}>
+              <div className="primaryView">
+                <label className="completed-control">
+                  <input
+                    type="checkbox"
+                    checked={task.completed}
+                    onChange={() => handleCompleted(task.id)}
+                  />
+                </label>
+                <h2>{task.title}</h2>
+              </div>
+              <div className="secondaryView">
+                <p>Due {prettyDate(task.dueDate)}</p>
+                <p>{task.priority} priority</p>
+                <p>{task.category}</p>
+              </div>
               <p>{task.description}</p>
-              <p>{prettyDate(task.dueDate)}</p>
-              <p>Priority: {task.priority}</p>
-              <p>Category: {task.category}</p>
 
-              <button
-                disabled={task.id === editId}
-                type="button"
-                onClick={() => handleDelete(task.id)}
-              >
-                Delete
-              </button>
-              <button
-                disabled={editId}
-                type="button"
-                onClick={() => handleEdit(task.id)}
-              >
-                Edit
-              </button>
+              <div className="buttonContainer">
+                {task.description && (
+                  <button type="button">View Description</button>
+                )}
+
+                <button
+                  disabled={task.id === editId}
+                  type="button"
+                  onClick={() => handleDelete(task.id)}
+                >
+                  Delete
+                </button>
+                <button
+                  disabled={editId}
+                  type="button"
+                  onClick={() => handleEdit(task.id)}
+                >
+                  Edit
+                </button>
+              </div>
             </li>
           ))}
         </ul>
