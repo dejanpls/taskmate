5d0328d621dacbb1d9bbfc84ee2a40997f625a76
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-09-14 10:03:32 +0200

    style(Tabs): tweak positioning, padding and font size

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,21 +1,26 @@
+import '../styles/tabs.css';
+
 export default function Tabs({ setShowDeleted, handleFilter }) {
   return (
-    <div>
-      <button onClick={handleFilter} value="all" type="button">
-        All
-      </button>
-      <button onClick={handleFilter} value="active" type="button">
-        Active
-      </button>
-      <button onClick={handleFilter} value="done" type="button">
-        Done
-      </button>
+    <div className="filterControls">
+      <div className="tabsContainer">
+        <button onClick={handleFilter} value="all" type="button">
+          All
+        </button>
+        <button onClick={handleFilter} value="active" type="button">
+          Active
+        </button>
+        <button onClick={handleFilter} value="done" type="button">
+          Done
+        </button>
+      </div>
       <button
+        className="material-icons"
         onClick={() => setShowDeleted((prev) => !prev)}
         value="deleted"
         type="button"
       >
-        Deleted
+        delete
       </button>
     </div>
   );
